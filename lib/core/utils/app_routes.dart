8820c40cd03565d38824d36114ae217766da2315
nguyentu-926 <119412483+nguyentu-926@users.noreply.github.com>
2025-03-18 08:42:51 +0700

    commit

--- lib/core/utils/app_routes.dart
+++
@@ -1,7 +0,0 @@
-class AppRoutes {
-  static const homeScreen = "/";
-  static const allTicketsScreen = "/all_tickets_screen";
-  static const ticketScreen = "/ticket_screen";
-  static const allHotelsScreen = "/all_hotels_screen";
-  static const hotelDetailsScreen = "/hotel_details_screen";
-}
--- lib/core/utils/constants.dart
+++
@@ -1,4 +0,0 @@
-class Constants {
-  static const String defaultCurrency = "DZD";
-  static const String successMessage = "You will be contacted by us very soon.";
-}
